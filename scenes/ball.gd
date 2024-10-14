84fe6a89fcac687436c15aa1d21f25201ba64767
Anderson Madeira <andersonmadeiracs@gmail.com>
2024-10-13 19:45:21 -0700

    Make the ball bounce off when colliding with walls or paddle

--- scenes/ball.gd
+++ scenes/ball.gd
@@ -3,14 +3,12 @@ class_name Ball
 
 @export var min_start_angle := 30.0
 @export var max_start_angle := 70.0
+@export var speed := 15.0
 
 @onready var debug_mesh: MeshInstance3D = $DebugMesh
 
-const SPEED = 15.0
-
 var rand := RandomNumberGenerator.new()
-
-var _direction := Vector3.ZERO
+var _velocity := Vector3.ZERO
 
 func _ready() -> void:
 	_reset()
@@ -25,20 +23,25 @@ func _reset() -> void:
 	global_position.x = 0
 
 	# debug_mesh.global_position = _get_random_direction() * 5
-	_direction = _get_random_direction()
+	var direction = _get_random_direction()
+
+	_velocity.x = direction.x * speed
+	_velocity.z = direction.z * speed
 
 func _get_random_direction() -> Vector3:
 	var xDirection = -1 if rand.randf() >= 0.5 else 1
 	var yDirection = -1 if rand.randf() >= 0.5 else 1
 	var angle = rand.randf_range(min_start_angle, max_start_angle)
 
+	# Get x,y coords given the angle
 	var x = cos(deg_to_rad(angle)) * xDirection
 	var y = sin(deg_to_rad(angle)) * yDirection
 
 	return Vector3(x, global_position.y, y)
-	
-func _physics_process(_delta: float) -> void:
-	velocity.x = _direction.x * SPEED
-	velocity.z = _direction.z * SPEED
 
-	move_and_slide()
+func _physics_process(delta: float) -> void:
+	var collision = move_and_collide(_velocity * delta)
+
+	# If we collided with something, bounce off
+	if collision:
+		_velocity = _velocity.bounce(collision.get_normal())
