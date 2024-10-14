0e071bc3221815a19e1c8bb6cd6898edd76b6da2
Anderson Madeira <andersonmadeiracs@gmail.com>
2024-10-13 19:57:43 -0700

    Make paddles through player input

---
+++ scripts/paddle.gd
@@ -0,0 +1,29 @@
+extends CharacterBody3D
+class_name Paddle
+
+@export var move_up_action: String = ''
+@export var move_down_action: String = ''
+@export var speed := 15.0
+
+var direction := 0
+
+func _physics_process(_delta: float) -> void:
+	var dir = get_movement_direction()
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	if dir:
+		velocity.x = dir * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+
+	move_and_slide()
+
+func get_movement_direction() -> int:
+	if Input.is_action_pressed(move_up_action):
+		return -1
+	
+	if Input.is_action_pressed(move_down_action):
+		return 1
+
+	return 0
